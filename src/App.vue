fa3f7bee19917307de05420f881f8c69cd72597d
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-10-29 18:08:36 +0600

    Set up a data centralized store

--- src/App.vue
+++ src/App.vue
@@ -20,14 +20,7 @@ export default {
     ProductListTwo,
   },
   data() {
-    return {
-      products: [
-        { name: "Banana Skin", price: 20 },
-        { name: "Shiny Star", price: 40 },
-        { name: "Green Shells", price: 60 },
-        { name: "Red Shells", price: 80 },
-      ],
-    };
+    return {};
   },
 };
 </script>
