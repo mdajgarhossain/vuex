8e87e1aff19ab52e2b77efec6492ffddcf0e3379
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-10-29 17:19:50 +0600

    Passing data as props from parent to child & use data by vuejs style

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,32 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/> -->
+    <ProdctListOne :products="products" />
+    <ProductListTwo :products="products" />
   </div>
 </template>
+
+<script>
+import ProdctListOne from "./components/ProdctListOne";
+import ProductListTwo from "./components/ProductListTwo";
+
+export default {
+  components: {
+    ProdctListOne,
+    ProductListTwo,
+  },
+  data() {
+    return {
+      products: [
+        { name: "Banana Skin", price: 20 },
+        { name: "Shiny Star", price: 40 },
+        { name: "Green Shells", price: 60 },
+        { name: "Red Shells", price: 80 },
+      ],
+    };
+  },
+};
+</script>
 
 <style>
 </style>
